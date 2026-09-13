c06e03fdc943fe5e1f0dca717b890a4ce0c9a925
lumens10 <151405791+lumens10@users.noreply.github.com>
2026-09-12 19:01:52 -0600

    main.js

--- js/main.js
+++ js/main.js
@@ -718,7 +718,7 @@ if (
                    entra en la zona de observación.
                 */
 
-                threshold: 0.05,
+                threshold: 0.10,
 
 
                 /*
@@ -729,7 +729,7 @@ if (
                 */
 
                 rootMargin:
-                    "10% 0px 10% 0px"
+                    "-5% 0px -5% 0px"
 
             }
 
